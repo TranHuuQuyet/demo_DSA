01e4aa8ba66dbe3f29dfcabda8901337b429fa6e
Nguyen Gia <gtran7085@gmail.com>
2025-11-20 13:41:22 +0700

    thêm comment để code dể hiểu

--- structs.h
+++ structs.h
@@ -1,35 +1,35 @@
 // structs.h
-#pragma once
-#include <string>
+#pragma once // đảm bảo file chỉ được include 1 lần 
+#include <string> //  khai báo thư viện string
 
 // 1. Dữ liệu cho Bảng Băm (Kho thuốc)
-struct MedicineInfo {
-    std::string maThuoc;
-    std::string tenThuoc;
-    std::string donViTinh;
-    double donGia;
+struct MedicineInfo { //khai báo các thuộc tính của 1 loại thuốc sử dụng bảng băm
+    std::string maThuoc; // mã thuốc
+    std::string tenThuoc; // tên thuốc
+    std::string donViTinh; // đơn vị tính
+    double donGia; // đơn giá
 };
 
 // 2. Dữ liệu cho Danh sách Liên kết (Thuốc trong 1 đơn)
-struct PrescriptionItemNode {
-    std::string maThuoc;
-    int soLuong;
-    std::string huongDanSuDung;
-    PrescriptionItemNode* next;
+struct PrescriptionItemNode { // khai báo thuộc tính của 1 loại thuốc trong đơn dùng Linked list
+    std::string maThuoc; // mã thuốc
+    int soLuong; // số lượng
+    std::string huongDanSuDung; // hướng dẫn sử dụng
+    PrescriptionItemNode* next; // con trỏ để trỏ dến thuốc tiếp theo trong 1 đơn
 };
 
 // Khai báo trước (forward declaration)
-class PrescriptionList; 
+class PrescriptionList; // khai báo để sử dụng con trỏ 
 
 // 3. Dữ liệu cho Đơn thuốc (Quản lý các DSLK)
-struct Prescription {
-    std::string maDonThuoc;
-    std::string maBenhNhan;
-    std::string ngayKeDon;
+struct Prescription { // khai báo thuộc tính của 1 đơn thuốc
+    std::string maDonThuoc; // mã đơn thuốc
+    std::string maBenhNhan; // mã bệnh nhân
+    std::string ngayKeDon; // ngày kê đơn
     
     PrescriptionList* chiTietThuoc; // Dùng con trỏ để quản lý
 
     // Chỉ KHAIBÁO
-    Prescription();
-    ~Prescription();
+    Prescription(); // Contructor khởi tạo giá trị ban đầu
+    ~Prescription(); // Destructor hàm hủy và giải phóng tài nguyên
 };
